6b260a1b53d65e558d35ec602e45ccbdd6f25851
Ngo ba phong <phongnbpd08162@fpt.edu.vn>
2023-03-09 10:46:51 +0700

    up code

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,8 +1,23 @@
-let a =prompt("Moi nhap so nguyen:");
+let a =prompt("Moi nhap so nguyen a :");
 let a1=parseInt(a);
-let b=prompt("Moi nhap so nguyen b:");
+let b=prompt("Moi nhap so nguyen b :");
 let b1=parseInt(b);
 
 let c=a1+b1;
 //let x=a+b;
 document.write(c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
